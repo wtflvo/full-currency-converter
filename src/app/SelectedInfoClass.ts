bfcfa5369e98cbfdb744ed56589bf4d35a0c3672
wtflvo <lutskovvo@gmail.com>
2022-10-17 21:13:07 +0300

    code of app

---
+++ src/app/SelectedInfoClass.ts
@@ -0,0 +1,6 @@
+export interface SelectedInfoClass {
+  currencyAIndex: number;
+  currencyBIndex: number;
+  quantityA: number;
+  quantityB: number;
+}
---
+++ src/app/currency-converter-forms/currency-converter-forms.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+import { CurrenciesInfo } from '../currencies-api.service';
+import { SelectedInfoClass } from '../SelectedInfoClass';
+
+@Component({
+  selector: 'app-currency-converter-forms',
+  templateUrl: './currency-converter-forms.component.html',
+  styleUrls: ['./currency-converter-forms.component.css'],
+  providers: [CurrenciesInfo],
+})
+export class CurrencyConverterFormsComponent implements OnInit {
+  constructor(
+    private currenciesInfo: CurrenciesInfo 
+  ) {}
+  currencies = this.currenciesInfo.getCurrenciesValue('long');
+
+  selectedInfo: SelectedInfoClass = {
+    currencyAIndex: 3,
+    currencyBIndex: 0,
+    quantityA: 0,
+    quantityB: 0,
+  };
+
+  public convertFirstInputs(
+    quantity: number = this.selectedInfo.quantityA,
+    primaryCurrencyIndex: number = this.selectedInfo.currencyAIndex,
+    secondaryCurrencyIndex: number = this.selectedInfo.currencyBIndex
+  ) {
+    if (quantity) {
+      return Number(
+        (quantity * this.currencies[primaryCurrencyIndex].buy) /
+          this.currencies[secondaryCurrencyIndex].buy
+      ).toFixed(2);
+    } else return;
+  }
+  convertedA = this.convertFirstInputs();
+
+  public convertSecondInputs(
+    quantity: number = this.selectedInfo.quantityB,
+    primaryCurrencyIndex: number = this.selectedInfo.currencyBIndex,
+    secondaryCurrencyIndex: number = this.selectedInfo.currencyAIndex
+  ) {
+    if (quantity) {
+      return Number(
+        (quantity * this.currencies[primaryCurrencyIndex].buy) /
+          this.currencies[secondaryCurrencyIndex].buy
+      ).toFixed(2);
+    } else return;
+  }
+  convertedB = this.convertSecondInputs();
+
+  changeInputA(UpdatedValue: number): void {
+    this.selectedInfo.quantityA = UpdatedValue;
+  }
+  changeInputB(UpdatedValue: number): void {
+    this.selectedInfo.quantityB = UpdatedValue;
+  }
+  changeIndexA(UpdatedValue: number): void {
+    this.selectedInfo.currencyAIndex = UpdatedValue;
+  }
+  changeIndexB(UpdatedValue: number): void {
+    this.selectedInfo.currencyBIndex = UpdatedValue;
+  }
+
+  ngOnInit(): void {}
+}
